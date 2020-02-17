9436d709f771983689b8ce4c37114f45ab53db6d
Grmcrkrs <grmcrkrs@gmail.com>
2020-02-16 22:16:21 -0600

    added color to the background of the text fields in the listview. fixed the blank space in phrases with no imageview. added visual polish.

--- app/src/main/java/com/example/android/miwok/Word.java
+++ app/src/main/java/com/example/android/miwok/Word.java
@@ -8,13 +8,37 @@ public class Word {
 
     private String mMiwokTranslation;
 
-    private String mImageResourceID;
+    private int mImageResourceID;
 
+    /**
+     * Image resource ID for the word
+     */
+    private int mImageResourceId = NO_IMAGE_PROVIDED;
+
+    /**
+     * Constant value that represents no image was provided for this word
+     */
+    private static final int NO_IMAGE_PROVIDED = -1;
+
+    /**
+     * public constructor with two inputs into the array.
+     *
+     * @param defaultTranslation
+     * @param miwokTranslation
+     */
     public Word(String defaultTranslation, String miwokTranslation) {
         mDefaultTranslation = defaultTranslation;
         mMiwokTranslation = miwokTranslation;
     }
-    public Word(String defaultTranslation, String miwokTranslation, String imageResourceID) {
+
+    /**
+     * public constructor with three inputs into the Word array, one of those being an image resource ID.
+     *
+     * @param defaultTranslation the default translation of the word, i.e. English
+     * @param miwokTranslation   the Miwok translation of the word
+     * @param imageResourceID    a String of the R.drawable... resource ID.
+     */
+    public Word(String defaultTranslation, String miwokTranslation, int imageResourceID) {
         mDefaultTranslation = defaultTranslation;
         mMiwokTranslation = miwokTranslation;
         mImageResourceID = imageResourceID;
@@ -34,5 +58,14 @@ public class Word {
         return mMiwokTranslation;
     }
 
-    public String getImageResourceID() { return mImageResourceID; }
+    public int getImageResourceID() {
+        return mImageResourceID;
+    }
+
+    /**
+     * Returns whether or not there is an image for this word.
+     */
+    public boolean hasImage() {
+        return mImageResourceId != NO_IMAGE_PROVIDED;
+    }
 }
--- app/src/main/java/com/example/android/miwok/WordAdapter.java
+++ app/src/main/java/com/example/android/miwok/WordAdapter.java
@@ -1,25 +1,30 @@
 package com.example.android.miwok;
 
 import android.app.Activity;
+import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
-//import android.widget.ImageView;
+import android.widget.ImageView;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
 
 public class WordAdapter extends ArrayAdapter<Word> {
+    private int mColorID;
+
 
     private static final String LOG_TAG = WordAdapter.class.getSimpleName();
 
-    public WordAdapter(Activity context, ArrayList<Word> androidFlavors) {
+    public WordAdapter(Activity context, ArrayList<Word> androidFlavors, int colorID) {
         // Here, we initialize the ArrayAdapter's internal storage for the context and the list.
         // the second argument is used when the ArrayAdapter is populating a single TextView.
         // Because this is a custom adapter for two TextViews and an ImageView, the adapter is not
         // going to use this second argument, so it can be any value. Here, we used 0.
         super(context, 0, androidFlavors);
+        mColorID = colorID;
     }
 
     /**
@@ -40,25 +45,45 @@ public class WordAdapter extends ArrayAdapter<Word> {
                     R.layout.list_item, parent, false);
         }
         // Get the {@link AndroidFlavor} object located at this position in the list
-        Word currentTranslation = getItem(position);
+        Word currentWord = getItem(position);
 
         // Find the TextView in the list_item.xml layout with the ID miwokTextView
         TextView miwokTextView = (TextView) listItemView.findViewById(R.id.miwokTextView);
         // Get the version name from the current AndroidFlavor object and
         // set this text on the name TextView
-        miwokTextView.setText(currentTranslation.getMiwokTranslation());
+        miwokTextView.setText(currentWord.getMiwokTranslation());
 
         // Find the TextView in the list_item.xml layout with the ID defaultTextView
         TextView defaultTextView = (TextView) listItemView.findViewById(R.id.defaultTextView);
         // Get the version number from the current AndroidFlavor object and
         // set this text on the number TextView
-        defaultTextView.setText(currentTranslation.getDefaultTranslation());
+        defaultTextView.setText(currentWord.getDefaultTranslation());
 
-//        // Find the ImageView in the list_item.xml layout with the ID list_item_icon
-//        ImageView iconView = (ImageView) listItemView.findViewById(R.id.list_item_icon);
-//        // Get the image resource ID from the current AndroidFlavor object and
-//        // set the image to iconView
-//        iconView.setImageResource(currentTranslation.getImageResourceId());
+        // Find the ImageView in the list_item.xml layout with the ID list_item_icon
+        ImageView imageView = (ImageView) listItemView.findViewById(R.id.imageView1);
+        // Get the image resource ID from the current Word object and
+        // set the image to imageView
+
+        //if the current Word object has an image, then it sets the ImageView to VISABLE.
+        //if the current Word object does not have an image, then it sets it to visability GONE.
+        /**TODO: ask someone about the following code and .hasImage() being called on currentWord
+         * TODO: for that matter, how can .getImageResourceID be called on currentWord?
+         */
+        if (currentWord.hasImage()) {
+            // If an image is available, display the provided image based on the resource ID
+            imageView.setImageResource(currentWord.getImageResourceID());
+            // Make sure the view is visible
+            imageView.setVisibility(View.VISIBLE);
+        } else {
+            // Otherwise hide the ImageView (set visibility to GONE)
+            imageView.setVisibility(View.GONE);
+        }
+        // Set the theme color for the list item
+        View textContainer = listItemView.findViewById(R.id.text_box);
+        // Find the color that the resource ID maps to
+        int color = ContextCompat.getColor(getContext(), mColorID);
+        // Set the background color of the text container View
+        textContainer.setBackgroundColor(color);
 
         // Return the whole list item layout (containing 2 TextViews and an ImageView)
         // so that it can be shown in the ListView
