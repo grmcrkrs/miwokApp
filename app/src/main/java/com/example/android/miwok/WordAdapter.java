65c223dfa9eec83e8837f1aa2db9c28c94daf655
grmcrkrs <grmcrkrs@gmail.com>
2020-02-21 15:23:42 -0600

    Added polish and fixed the padding, still has blank image in phrases activity

--- app/src/main/java/com/example/android/miwok/WordAdapter.java
+++ app/src/main/java/com/example/android/miwok/WordAdapter.java
@@ -1,6 +1,7 @@
 package com.example.android.miwok;
 
 import android.app.Activity;
+import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,16 +12,21 @@ import android.widget.TextView;
 
 import java.util.ArrayList;
 
+
 public class WordAdapter extends ArrayAdapter<Word> {
+    private int mColorID;
+
 
     private static final String LOG_TAG = WordAdapter.class.getSimpleName();
 
-    public WordAdapter(Activity context, ArrayList<Word> androidFlavors) {
+    public WordAdapter(Activity context, ArrayList<Word> androidFlavors, int colorID) {
         // Here, we initialize the ArrayAdapter's internal storage for the context and the list.
         // the second argument is used when the ArrayAdapter is populating a single TextView.
         // Because this is a custom adapter for two TextViews and an ImageView, the adapter is not
         // going to use this second argument, so it can be any value. Here, we used 0.
         super(context, 0, androidFlavors);
+        mColorID = colorID;
+
     }
 
     /**
@@ -63,9 +69,14 @@ public class WordAdapter extends ArrayAdapter<Word> {
 //        // Get the image resource ID from the current AndroidFlavor object and
 //        // set the image to iconView
 //        iconView.setImageResource(currentTranslation.getImageResourceId());
-
+        View textContainer = listItemView.findViewById(R.id.text_box);
+        // Find the color that the resource ID maps to
+        int color = ContextCompat.getColor(getContext(), mColorID);
+        // Set the background color of the text container View
+        textContainer.setBackgroundColor(color);
         // Return the whole list item layout (containing 2 TextViews and an ImageView)
         // so that it can be shown in the ListView
         return listItemView;
     }
+
 }
